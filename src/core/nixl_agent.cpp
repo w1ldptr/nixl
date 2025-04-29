548f2d34a259b0af7790f532bdcb206472b3a74e
Vlad Buslov <vladbu@nvidia.com>
2025-04-29 22:10:19 +0300

    Extend UCX backend to support multiple workers
    Configure number of workers by setting new agent config option
    'numWorkers'.
    
    Spawn multiple workers in UCX backend and establish connection on all of
    them but decide which instance to use for data-path operations based on
    caller thread id hash to allow equal spread of tasks over multiple workers
    in a way that is transparent to the user.
    
    Make memory and rkey helpers in nixlUcxWorker static since they work on
    context that is shared between all worker instances as opposed to
    per-worker. Leaving them as nixlUcxWorker members is a pragmatic decision
    to avoid re-designing nixlUcxContext and nixlUcxMem classes that currently
    mark all of their fields private and only expose it to nixlUcxWorker
    'friend' class.
    
    For the purposes of generating metadata the last created worker is always
    chosen. We need to chose one and using the last one allows most
    straightforward implementation.
    
    Signed-off-by: Vlad Buslov <vladbu@nvidia.com>

--- src/core/nixl_agent.cpp
+++ src/core/nixl_agent.cpp
@@ -209,6 +209,7 @@ nixlAgent::createBackend(const nixl_backend_t &type,
     init_params.customParams = const_cast<nixl_b_params_t*>(&params);
     init_params.enableProgTh = data->config.useProgThread;
     init_params.pthrDelay    = data->config.pthrDelay;
+    init_params.numWorkers   = data->config.numWorkers;
 
     // First, try to load the backend as a plugin
     auto& plugin_manager = nixlPluginManager::getInstance();
